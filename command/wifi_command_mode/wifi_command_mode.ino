0f52b7f065b2a5445b1943cfad1b8d01b1750f9b
KD8BXP <kd8bxp@aol.com>
2015-09-08 00:42:31 -0400

    updated WIFI command sketch

--- command/wifi_command_mode/wifi_command_mode.ino
+++ command/wifi_command_mode/wifi_command_mode.ino
@@ -1,5 +1,51 @@
 #define DEBUG true
- 
+
+// Motor Controls Link to Original Article
+// Automation Technology Club
+// Motor Controls
+// Free to use as you see if - Pin Numbering by Grand Poobah
+// Subroutines Aug 18 - LeRoy Miller
+// additions will be added for speed correction of wheels
+
+const int PWMLPin = 44; //	Enable PWM Left Motor
+const int PWMRPin = 46; //	Enable PWM Right Motor
+const int L1Pin = 36; // 	L1 = Left Motor Lead 1
+const int L2Pin = 38; // 	L2 = Left Motor Lead 2
+const int L3Pin = 40; // 	L3 = Right Motor Lead 1
+const int L4Pin = 42; // 	L4 = Right Motor Lead 2
+const int LEDPin = 13; // LED
+const int MaxSpeed = 255; //not yet used, this will be the max speed your motors can drive at (max PWM) and will be used to help correct for differences in motors
+const int CorrectLeft = 0; //to be added later, this will attempt to correct for any motor differances in going forward/backward
+const int CorrectRight = 0; //to be added later, this will attempt to correct for any motor differances in going forward/backward
+int speed; 
+
+/* FUNCTION CALL USES
+lefttight();  //turn in place to the left
+righttight(); //turn in place to the right
+left();   //turn left going forward
+leftb();  //turn left going backward
+allstop(); //stop all motors and pwm, small delay for everything to catch up
+right(); //turn right going forward
+rightb(); //turn right going backward
+forward(); //move the robot forward
+backward(); // move the robot backward
+circleleft(100); //make a left hand circle X number of times
+circleright(100); //make a right hand circle  X number of times
+*/
+
+/*Listens for a code being sent from a webpage, the codes are used to drive the BittyBot
+Codes come in the form of: http://192.168.4.1/?drive=XX where XX is a number for the command.
+Commands:
+10 - Tight Left Turn
+11 - Tight Right Turn
+12 - Left Turn
+13 - Right Turn
+14 - Backward Left
+15 - Backward Right
+16 - Forward
+17 - Backward
+18 - All Stop
+*/
 
 void setup()
 {
@@ -14,11 +60,19 @@ void setup()
   sendData("AT+CIFSR\r\n",1000,DEBUG); // get ip address
   sendData("AT+CIPMUX=1\r\n",1000,DEBUG); // configure for multiple connections
   sendData("AT+CIPSERVER=1,80\r\n",1000,DEBUG); // turn on server on port 80
+  
+  pinMode (L1Pin, OUTPUT);
+pinMode (L2Pin, OUTPUT);
+pinMode (L3Pin, OUTPUT);
+pinMode (L4Pin, OUTPUT);
+pinMode (PWMLPin, OUTPUT);
+pinMode (PWMRPin, OUTPUT);
+  
 }
  
 void loop()
 {
-	//startwww();
+  speed = 75;	
 	listenwww();
 }
 
@@ -42,6 +96,47 @@ void listenwww() {
      Serial.print("Found: ");
      Serial.println(pinNumber);
      
+     switch (pinNumber) {
+       
+       case 10:
+       lefttight();
+       break;
+       
+       case 11:
+       righttight();
+       break;
+       
+       case 12:
+       left();
+       break;
+       
+       case 13:
+       right();
+       break;
+       
+       case 14:
+       leftb();
+       break;
+       
+       case 15:
+       rightb();
+       break;
+       
+       case 16:
+       forward();
+       break;
+       
+       case 17:
+       backward();
+       break;
+       
+       case 18:
+       allstop();
+       break;
+       
+       default:
+       break;
+     }
      // make close command
      String closeCommand = "AT+CIPCLOSE="; 
      closeCommand+=connectionId; // append connection id
@@ -52,66 +147,6 @@ void listenwww() {
   }
 }
 
-void startwww() {	
-  if(Serial1.available()) // check if the esp is sending a message 
-  {
-    /*
-    while(Serial1.available())
-    {
-      // The esp has data so display its output to the serial window 
-      char c = Serial1.read(); // read the next character.
-      Serial.write(c);
-    } */
-    
-    if(Serial1.find("+IPD,"))
-    {
-     delay(1000);
- 
-     int connectionId = Serial1.read()-48; // subtract 48 because the read() function returns 
-                                           // the ASCII decimal value and 0 (the first decimal number) starts at 48
-     
-     String webpage = "<h1>Bitty Bot</h2><br>Ready<br><br><form method=\"get\"><input type=\"submit\" value=\"11\" name=\"drive\">FORWARD<br><br>";
- 
-     String cipSend = "AT+CIPSEND=";
-     cipSend += connectionId;
-     cipSend += ",";
-     cipSend +=webpage.length();
-     cipSend +="\r\n";
-     
-     sendData(cipSend,1000,DEBUG);
-     sendData(webpage,1000,DEBUG);
-     
-     webpage="<input type=\"submit\" value=\"12\" name=\"drive\">LEFT<br><br><input type=\"submit\" value=\"13\" name=\"drive\">RIGHT<br><br>";
-     
-     cipSend = "AT+CIPSEND=";
-     cipSend += connectionId;
-     cipSend += ",";
-     cipSend +=webpage.length();
-     cipSend +="\r\n";
-     
-     sendData(cipSend,1000,DEBUG);
-     sendData(webpage,1000,DEBUG);
- 
-  webpage="<input type=\"submit\" value=\"14\" name=\"drive\">BACKWARD<br><br></form>";
-     
-     cipSend = "AT+CIPSEND=";
-     cipSend += connectionId;
-     cipSend += ",";
-     cipSend +=webpage.length();
-     cipSend +="\r\n";
-     
-     sendData(cipSend,1000,DEBUG);
-     sendData(webpage,1000,DEBUG);
-    
-     String closeCommand = "AT+CIPCLOSE="; 
-     closeCommand+=connectionId; // append connection id
-     closeCommand+="\r\n";
-     
-     sendData(closeCommand,3000,DEBUG);
-    }
-  }
-}
- 
  
 String sendData(String command, const int timeout, boolean debug)
 {
@@ -138,4 +173,104 @@ String sendData(String command, const int timeout, boolean debug)
     }
     
     return response;
+}
+
+void lefttight() {
+	digitalWrite (L1Pin, 1);
+	digitalWrite (L2Pin, 0);
+	analogWrite (PWMLPin, speed);
+	digitalWrite (L3Pin, 0);
+	digitalWrite (L4Pin, 1);
+	analogWrite (PWMRPin, speed);
+}
+
+void left() {
+	digitalWrite (L1Pin, 0);
+	digitalWrite (L2Pin, 0);
+	digitalWrite (L3Pin, 0);
+	digitalWrite (L4Pin, 1);
+	analogWrite(PWMLPin, 0);
+	analogWrite(PWMRPin, speed);
+	
+}
+
+void right() {
+	digitalWrite (L1Pin, 0);
+	digitalWrite (L2Pin, 1);
+	digitalWrite (L3Pin, 0);
+	digitalWrite (L4Pin, 0);
+	analogWrite(PWMLPin, speed);
+	analogWrite(PWMRPin, 0);
+}
+
+void leftb() {
+	digitalWrite (L1Pin, 0);
+	digitalWrite (L2Pin, 0);
+	digitalWrite (L3Pin, 1);
+	digitalWrite (L4Pin, 0);
+	analogWrite(PWMLPin, 0);
+	analogWrite(PWMRPin, speed);
+}
+
+void rightb() {
+	digitalWrite (L1Pin, 1);
+	digitalWrite (L2Pin, 0);
+	digitalWrite (L3Pin, 0);
+	digitalWrite (L4Pin, 0);
+	analogWrite(PWMLPin, speed);
+	analogWrite(PWMRPin, 0);
+}
+int righttight() {
+	digitalWrite(L1Pin, 0);
+	digitalWrite(L2Pin, 1);
+	analogWrite (PWMLPin, speed);
+	digitalWrite(L3Pin, 1);
+	digitalWrite(L4Pin, 0);
+	analogWrite(PWMRPin, speed);
+}
+
+int backward() {
+	digitalWrite(L1Pin, 1);
+	digitalWrite(L2Pin, 0);
+	digitalWrite(L3Pin, 1);
+	digitalWrite(L4Pin, 0);
+	analogWrite(PWMLPin, speed);
+	analogWrite(PWMRPin, speed);
+}
+
+int forward() {
+	digitalWrite(L1Pin, 0);
+	digitalWrite(L2Pin, 1);
+	digitalWrite(L3Pin, 0);
+	digitalWrite(L4Pin, 1);
+	analogWrite(PWMLPin, speed);
+	analogWrite(PWMRPin, speed);
+	
+}
+
+int allstop() {
+	digitalWrite(L1Pin, 0);
+	digitalWrite(L2Pin, 0);
+	digitalWrite(L3Pin, 0);
+	digitalWrite(L4Pin, 0);
+	analogWrite(PWMLPin, 0);
+	analogWrite(PWMRPin, 0);
+delay(500);
+
+}
+
+int circleleft(int count) {
+	for (int i=0; i<=count; i++) {
+		left();
+	delay(250);	
+	}
+	allstop();
+}
+
+int circleright(int count) {
+	for (int i=0; i<=count; i++) {
+		right();
+		delay(250);
+	}
+	allstop();
 }
