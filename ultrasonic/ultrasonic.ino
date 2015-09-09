f30775d42ac74778fe2093c954581bf458de85f6
KD8BXP <kd8bxp@aol.com>
2015-09-09 15:35:17 -0400

    Added voltage sensor and OLED support

--- ultrasonic/ultrasonic.ino
+++ ultrasonic/ultrasonic.ino
@@ -35,6 +35,22 @@ circleleft(100); //make a left hand circle X number of times
 circleright(100); //make a right hand circle  X number of times
 */
 
+//Sept 8 - Added Voltage Sensor with Shut off at 6.2volts
+
+int val11; //for voltage sensor
+int val2; //for voltage sensor
+float volts; //for voltage sensor
+
+//Sept 8 - Added OLED Screen for Display information
+//Needed for OLED
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
 #define TRIGGER_PIN 12 //Arduino Pin Tied to Trigger Pin on the ultrasonic sensor
 #define ECHO_PIN 11 //Arduino Pin Tied to echo pin on the ultrasonic sensor
 #define MAX_DISTANCE 200 //Maximum distance we want to ping for (in centimeters).  Maximum sensor distance is rated at 400 - 500cm
@@ -53,12 +69,44 @@ pinMode (L4Pin, OUTPUT);
 pinMode (PWMLPin, OUTPUT);
 pinMode (PWMRPin, OUTPUT);
 
+display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+display.clearDisplay();
+display.setTextSize(1);
+display.setTextColor(WHITE);
+display.setCursor(0,0);
+display.println(" Bitty-Bot  Robot");
+display.println("Startup...");
+display.display();
+delay(2000);
+//display.clearDisplay();
+display.setCursor(0,8);
+display.setTextColor(BLACK);
+display.println("Startup...");
+display.setCursor(0,8);
+display.setTextColor(WHITE);
+display.println("Ready....");
+display.display();
+delay(2000);
+display.clearDisplay();
+Serial.begin(9600);
 
 }
 
 void loop()
 {
 
+display.setTextColor(BLACK);
+display.setCursor(0,46);
+display.print("Volts: ");
+display.print(volts);
+display.print(" V");
+volts = checkvoltage();
+display.setTextColor(WHITE);
+display.setCursor(0,46);
+display.print("Volts: ");
+display.print(volts);
+display.print(" V");
+display.display();
 
 speed = 150; // fastest I want Bitty Bot to go
   delay(500);
@@ -193,3 +241,49 @@ int circleright(int count) {
 	}
 	allstop();
 }
+
+int checkvoltage() {
+float temp;
+val11=analogRead(1);
+temp=(val11/4.092)/10;
+val11=(int)temp * 10;//
+val2=((val11%100)/10);
+Serial.print("Raw Reading: ");
+Serial.println(temp);
+Serial.println("Corrected Reading: ");
+Serial.println(val2);
+delay(1000);
+
+if (temp <= 7.5 || temp >=7.1) {
+	Serial.println("Very Low Voltage");
+	tone(9, 1000, 100);
+}
+
+if (temp <= 7.00) {
+	Serial.println("Voltage Crital");
+	sos();
+}
+return (temp);
+}
+
+int sos() {
+	
+	for (int xx=0; xx<3; xx++) {
+		tone(9, 440, 100);
+		delay(200);
+		noTone(9);
+	}
+	for (int xx=0; xx<3; xx++){
+		tone(9, 440, 300);
+		delay(400);
+		noTone(9);
+	}
+	for (int xx=0; xx<3; xx++) {
+		tone(9, 440, 100);
+		delay(200);
+		noTone(9);
+	}
+	delay(1000);
+	sos();
+}
+
